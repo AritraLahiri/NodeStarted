18c138bb40a5dedb107947e198bf4602a4c6ddc9
Aritra <aritralahiri17@gmail.com>
2023-10-28 13:33:38 +0530

    added server

---
+++ App.js
@@ -0,0 +1,3 @@
+const http = require("http");
+const server = http.createServer((req, res) => console.log("Aritra Lahiri"));
+server.listen(4000);
--- HelloNode.js
+++ HelloNode.js
@@ -1,5 +1,23 @@
-console.log("Hello World!");
-const fs = require("fs");
-fs.writeFileSync("ReadMe.txt", "Welcome from Node !");
-const product = (a,b) => a*b;
-console.log(product(4,2)); 
+// console.log("Hello World!");
+// const fs = require("fs");
+// fs.writeFileSync("ReadMe.txt", "Welcome from Node !");
+// const product = (a, b) => a * b;
+// console.log(product(4, 2));
+
+const printData = (val, time) => {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(console.log(val)), time);
+  });
+  promise.then((a) => a);
+  return promise;
+};
+
+const print = () => {
+  console.log("a");
+  console.log("b");
+  printData("c", 3000)
+    .then((a) => a)
+    .then(() => printData("d", 0))
+    .then(() => console.log("e"));
+};
+print();
