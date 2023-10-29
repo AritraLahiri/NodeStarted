55940c06f385ea146ebebee35e10c05e3fec9066
Aritra <aritralahiri17@gmail.com>
2023-10-29 11:37:22 +0530

    added dummy routes

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
 const http = require("http");
-const server = http.createServer((req, res) => console.log("Aritra Lahiri"));
+const server = http.createServer((req, res) => {
+  if (req.url === "/home") {
+    res.setHeader("content-type", "text/html");
+    res.write("Welcome Home");
+    res.end();
+  } else if (req.url === "/about") {
+    res.setHeader("content-type", "text/html");
+    res.write("Welcome to About Us");
+    res.end();
+  } else if (req.url === "/node") {
+    res.setHeader("content-type", "text/html");
+    res.write("Welcome to my NodeJs Project");
+    res.end();
+  }
+});
+
 server.listen(4000);
